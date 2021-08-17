a823bed143ec2d5d9ead3ab799e121890f20d0e7
i.shesholko <~rr?[gujCw9_'4Vh>
2021-08-17 12:54:26 +0300

    commit in dev and push

--- src/main/java/com/example/git/Test2.java
+++ src/main/java/com/example/git/Test2.java
@@ -3,6 +3,8 @@ package com.example.git;
 public class Test2 {
 
     public static void main(String[] args) {
+        System.out.println("two from dev");
+
         System.out.println("ONLY DEV");
     }
 
